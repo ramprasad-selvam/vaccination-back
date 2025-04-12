0aade77055d82311bd23b2052c91c6140aaab2e7
harishbabu-s <harishbabu-s@hcltech.com>
2025-04-12 15:10:59 +0530

    updated database

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -2,11 +2,11 @@ import mongoose, { Schema } from "mongoose";
 import { IUser } from "../interfaces/interface";
 
 
-const patientDetails = {
-  vaccinations : [],
-  allergies : [],
-  pastVaccinations : 
-}
+// const patientDetails = {
+//   vaccinations : [],
+//   allergies : [],
+//   pastVaccinations : 
+// }
 
 const UserSchema: Schema = new Schema({
   name: { type: String, required: true },
@@ -21,9 +21,7 @@ const UserSchema: Schema = new Schema({
     default: 'user',
     required: true,
   },
-  patientDetails : {
-    
-  }
+
 
 });
 
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,7 +2,7 @@ import { Router } from "express"
 
 const userRouter = Router();
 
-userRouter.put("/profile",  )
-userRouter.post("/vaccines",  )
+// userRouter.put("/profile",  )
+// userRouter.post("/vaccines",  )
 
 export default userRouter;
